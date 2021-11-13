77480eac22c2c495c314b0bcf63a3ec92643767c
gricosm <guillermo.rico.sm@gmail.com>
2021-11-13 15:35:19 +0100

    Added Rent's CRUD

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv').config()
+const app = express();
+const moviesRoutes = require('./Movies/routes')
+const userRoutes = require('./Users/routes')
+const rentRoutes = require('./Rents/routes')
+
+app.use(express.json()); 
+
+mongoose.connect('mongodb://localhost:27017/backend_netflix', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(() => console.log('Conectado a la BBDD.'))
+    .catch((e) => console.log(e))
+
+app.use('/user', userRoutes)
+app.use('/movies', moviesRoutes)
+app.use('/rent', rentRoutes)
+app.listen(process.env.PORT, () => console.log('Funcionando'))
+console.log(process.env.PORT)
+
+
