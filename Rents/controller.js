fa8d8b1577fdde11dbdd382975a319e41ef436a2
Raul <raaul9212@gmail.com>
2021-11-18 16:54:36 +0100

    Rent querey works

--- Rents/controller.js
+++ Rents/controller.js
@@ -1,1 +1,27 @@
 const rentData = require('./model.js')
+
+
+//
+
+module.exports.addMovie = async (req,res) =>{
+    try{
+        const userMovie = req.body
+        await rentData.create(userMovie)      
+        res.json({add: userMovie}) 
+    }catch{
+        res.send("no entra")
+    }
+}
+
+module.exports.getRent = async (req,res) =>{
+    
+    try{
+        const searchById= await rentData.find({idUser:req.query.idUser}) 
+        console.log(searchById)
+        res.json({data:searchById})
+    }catch{
+        res.send({tonto:"error"})
+    }
+}
+
+
